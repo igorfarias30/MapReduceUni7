bd65bc0f60bc7018ab1317cbfef3092bebc34aac
Igor Farias Sousa <igor_sousa@virgo.atlantico.net.br>
2018-08-27 11:03:09 -0300

    update readme

--- README.md
+++ README.md
@@ -1,23 +1,24 @@
 # Map e Reduce
 
+## Atividade 1
 Disciplina de Programação com Map e Reduce, ministrada pelo prof. Robério no curso de Pós em Ciência de Dados, na UNI7. Primeira [atividade](dataset\uni7-pod-map-reduce-exercise-01.pdf) da disciplina.
 
 Requerimentos
 * `Python 3.6`
 * `mrjob`
 
-## Run
+### Run
 
 Para rodar, irei supor que o user já possui o python instalado em máquina e com o `pip` setado na variável de ambiente.
 
-### Primeiro passo
+#### Primeiro passo
 
 Baixar as packages do MRjob:
 
 ```console
 user@computer:~$ pip install mrjob
 ```
-### Segundo passo
+#### Segundo passo
 
 Para rodar um sript, você deve executar o seguite comando:
 
@@ -27,8 +28,14 @@ user@computer:~$ python <script>.py dataset/<file>.txt >> output/<out>.txt
 
 Por exemplo, vamos executar o script que resolve o exercício 2:       
 
+
 ```console
 user@computer:~$ python MRPopularHero.py dataset/Marvel-graph.txt >> output/hero.txt
+```
+## Atividade 2
+
+```console
+user@computer:~$ python RecommendationFriends.py dataset/Marvel-graph.txt >> output/recomendations.txt
 ```
 
 Para mais detalhes dos métodos do MRjob:
--- RecommendationFriends.py
+++ RecommendationFriends.py
@@ -2,15 +2,17 @@ from mrjob.step import MRStep
 from mrjob.job import MRJob
 
 class RecommendationFriends(MRJob):
-    
+
     def mapper_friends(self, _, line):
 
         hero, friends = line.split()[0], line.split()[1:]
 
+        # mapping friends that have a friendship
         for friend in friends:
-            direct = (friend, -1)
+            direct = (friend, -1) # -1 means tha hero is a friend of friend[i], for all i in [1, ..., p]
             yield hero, direct
-        
+
+        # creting tuple of possible friends in hero friends
         for i in range(len(friends)):
             for j in range(i + 1, len(friends)):
 
@@ -20,17 +22,20 @@ class RecommendationFriends(MRJob):
                 possiibleFriend2 = (friends[i], hero)
                 yield friends[j], possiibleFriend2
 
+    # creating a list of suggest friends and show common friends
     def buildOutput(self, mutualFriends):
-        
+
         output = ""
         for key, values in mutualFriends.items():
+            #values = sorted(values, key = lambda v: len(v), reverse = True)
             if values != []:
                 output += key + " (" + str(len(values)) + ": " + str(values) + "), "
-        
+
         return output
 
+    # reducer to make mutualfriends
     def reducer(self, key, values):
-        
+
         mutualFriends = {}
         values = list(values)
 
@@ -42,15 +47,11 @@ class RecommendationFriends(MRJob):
                 elif mutualFriends[toUser] != []:
                     mutualFriends[toUser].append(mutualFriend)
             else:
-                if alreadyFriend:
-                    mutualFriends[toUser] = []
-                    
-                else:
-                    mutualFriends[toUser] = [mutualFriend]
-        
+                if alreadyFriend: mutualFriends[toUser] = []
+                else: mutualFriends[toUser] = [mutualFriend]
+
         reducerOutput = self.buildOutput(mutualFriends)
         yield key, reducerOutput
-        #yield key, mutualFriends
 
     def steps(self):
         return [MRStep(mapper = self.mapper_friends, reducer = self.reducer)]
